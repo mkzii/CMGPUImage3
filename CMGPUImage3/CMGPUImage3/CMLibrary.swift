a3ac88cbb66d3950e4fbdc96386886558bef02f5
mkzii <kziichiro@gmail.com>
2024-07-16 18:33:03 +0900

    add file to project

---
+++ CMGPUImage3/CMGPUImage3/CMLibrary.swift
@@ -0,0 +1,14 @@
+//
+//  CMLibrary.swift
+//  CMGPUImage3
+//
+//  Created by nguyen-van-hai on 2024/07/16.
+//
+
+import Foundation
+
+public class CMLibrary {
+    public class func displayMessage(message: String) {
+        print("Message:  \(message)")
+    }
+}
