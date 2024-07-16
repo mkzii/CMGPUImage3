7afa2e49ed5c3636fdde70548acb62ec427d9ec9
mkzii <kziichiro@gmail.com>
2024-07-16 14:07:09 +0900

    create project

---
+++ CMGPUImage3/CMGPUImage3/CMGPUImage3.h
@@ -0,0 +1,18 @@
+//
+//  CMGPUImage3.h
+//  CMGPUImage3
+//
+//  Created by nguyen-van-hai on 2024/07/16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CMGPUImage3.
+FOUNDATION_EXPORT double CMGPUImage3VersionNumber;
+
+//! Project version string for CMGPUImage3.
+FOUNDATION_EXPORT const unsigned char CMGPUImage3VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CMGPUImage3/PublicHeader.h>
+
+
